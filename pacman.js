5324e2ee00df6670de28f4f2ea4bfbf814260d41
ynjacobs <ytj64611@gamil.com>
2019-06-13 17:54:34 -0400

    pacman gamegit add -Agit add -Agit add -Agit add -A!

--- pacman.js
+++ pacman.js
@@ -1,6 +1,7 @@
 // Setup initial game stats
 let score = 0;
 let lives = 2;
+let powerPellet = 4;
 
 
 // Define your ghosts here
@@ -54,13 +55,19 @@ function clearScreen() {
 
 function displayStats() {
   console.log(`Score: ${score}     Lives: ${lives}`);
+  console.log(`Power-Pellets: ${powerPellet}`);
+  
 }
 
 function displayMenu() {
   console.log('\n\nSelect Option:\n');  // each \n creates a new line
   console.log('(d) Eat Dot');
+  if (powerPellet > 0) console.log('(p) Eat Power-Pellet');
+  
   for(i = 0; i < ghosts.length; i++){
-    console.log(`(${i+1}) Eat ${ghosts[i].name}`);
+    console.log(`(${i+1}) Eat ${ghosts[i].name} (${ghosts[i].edible? "Edible":"Not Edible"})`);
+    
+    
     
   }
   console.log('(q) Quit');
@@ -82,6 +89,27 @@ function eatDot() {
 // Process Player's Input
 function processInput(key) {
   switch(key) {
+    case 'p':
+    if (powerPellet > 0){
+      eatPowerPellet();
+    }else {
+      console.log('no more pellets left to eat, do not press p again please');
+      
+    }
+    break;
+      
+    case '1':
+      eatGhost(inky);
+      break;
+    case '2':
+      eatGhost(blinky);
+      break;
+    case '3':
+      eatGhost(pinky);
+      break;
+    case '4':
+      eatGhost(clyde);
+      break;
     case '\u0003': // This makes it so CTRL-C will quit the program
     case 'q':
       process.exit();
@@ -94,7 +122,37 @@ function processInput(key) {
   }
 }
 
+function eatGhost (ghost) {
+  if(ghost.edible){
+    console.log(`${ghost.name} has been eated, he was a mean ${ghost.character}!`);
+    score += 200;
+    ghost.edible = false;
+  }
+  else{
+    lives-- ;
+    console.log(`the ghost is ${ghost.name} and he is ${ghost.colour} and he ate pacman `);
+    
+    checkLives();
+    
+  }
+}
 
+function eatPowerPellet() {
+  score += 50;
+  for (index in ghosts){
+    ghosts[index].edible = true;
+  }
+  powerPellet--;
+}
+
+function checkLives() {
+  if (lives == 0) {
+    displayStats()
+
+    process.exit();
+    
+  }
+}
 //
 // YOU PROBABLY DON'T WANT TO CHANGE CODE BELOW THIS LINE
 //
@@ -112,7 +170,7 @@ drawScreen();
 stdin.on('data', (key) => {
   process.stdout.write(key);
   processInput(key);
-  setTimeout(drawScreen, 300); // The command prompt will flash a message for 300 milliseoncds before it re-draws the screen. You can adjust the 300 number to increase this.
+  setTimeout(drawScreen, 500); // The command prompt will flash a message for 300 milliseoncds before it re-draws the screen. You can adjust the 300 number to increase this.
 });
 
 // Player Quits
